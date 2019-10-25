7c69028fa2e91e44dfc4f161d4ea31cd90040f04
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-10-25 09:50:40 -0400

    Created the intro to the game

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,11 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is the Number Guessing game. To play guess any number betweeen 1 and 50");
+       
+  }
+}

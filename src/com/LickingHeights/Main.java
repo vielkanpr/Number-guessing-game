2080369b5967817d328fe990c696bbb0f7381a44
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-10-25 09:51:15 -0400

    initialized  both the guess and answer.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the Number Guessing game. To play guess any number betweeen 1 and 50");
-       
+        int guess;
+        int answer;
+
   }
 }

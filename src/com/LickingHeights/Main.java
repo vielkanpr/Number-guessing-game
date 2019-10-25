9dc8913bff715cf0bd0ea539f90937acdaaddcd8
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-10-25 10:00:45 -0400

    Changed wording and the range of numbers to choose from.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the Number Guessing game. To play guess any number betweeen 1 and 50");
+        System.out.println("This is the Number Guessing game. To play guess any number betweeen 0 and 75");
         int guess;
         int answer;
         answer = 21;
         do {
             Scanner keyboard;
             keyboard = new Scanner(System.in);
-            System.out.println(" please guess number now;");
+            System.out.println(" please guess a number now;");
             guess = keyboard.nextInt();
             if (guess > answer ){
-                System.out.println("that number is too high. Please try again.");
+                System.out.println("That number is too high. You can try again by inserting another number.");
             }
             else if (guess< answer){
-                System.out.println("that number is too low. Try again please.");
+                System.out.println("That number is too low. You can try again by inserting another number.");
             }
             else if (guess == answer){
-                System.out.println("Congratulations! You guessed the right number!");
+                System.out.println("Congratulations! You guessed exactly the right number!");
             }
     }
     while (guess > answer || guess < answer);
